39ab9eec6dbd8e5f309831641aef9e46ab80d7de
afilsaime <shadow_kungfu@hotmail.fr>
2016-05-15 12:45:41 +0200

    database

--- database_export/mysite.sql
+++ database_export/mysite.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 15 Mai 2016 à 12:17
+-- Généré le :  Dim 15 Mai 2016 à 12:42
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -20,48 +20,12 @@ SET time_zone = "+00:00";
 -- Base de données :  `mysite`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_group`
---
-
-CREATE TABLE `auth_group` (
-  `id` int(11) NOT NULL,
-  `name` varchar(80) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `auth_group`
 --
 
 INSERT INTO `auth_group` (`id`, `name`) VALUES
 (1, 'Artistes');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_group_permissions`
---
-
-CREATE TABLE `auth_group_permissions` (
-  `id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `permission_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_permission`
---
-
-CREATE TABLE `auth_permission` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `content_type_id` int(11) NOT NULL,
-  `codename` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `auth_permission`
@@ -102,26 +66,6 @@ INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALU
 (38, 'Can change album', 13, 'change_album'),
 (39, 'Can delete album', 13, 'delete_album');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_user`
---
-
-CREATE TABLE `auth_user` (
-  `id` int(11) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `last_login` datetime(6) DEFAULT NULL,
-  `is_superuser` tinyint(1) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `email` varchar(254) NOT NULL,
-  `is_staff` tinyint(1) NOT NULL,
-  `is_active` tinyint(1) NOT NULL,
-  `date_joined` datetime(6) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `auth_user`
 --
@@ -130,18 +74,6 @@ INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `userna
 (4, 'pbkdf2_sha256$24000$ybkT8sJqfry0$WZ5DIValYJIfe/phITNe/K1l3YTteuoaqAKQNF35K5s=', '2016-05-03 07:22:16.264000', 1, 'Admin', '', '', 'admin@site.com', 1, 1, '2016-05-02 18:03:18.169000'),
 (22, 'pbkdf2_sha256$24000$L3rfEEM0bfn5$Fj2k5Bg/w+f0D7oKhU8DbBZ747L8H4wgncfqNqPvVDc=', NULL, 0, 'toto', '', '', 'test@test.fr', 0, 1, '2016-05-03 07:38:41.077000');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_user_groups`
---
-
-CREATE TABLE `auth_user_groups` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `auth_user_groups`
 --
@@ -149,35 +81,6 @@ CREATE TABLE `auth_user_groups` (
 INSERT INTO `auth_user_groups` (`id`, `user_id`, `group_id`) VALUES
 (1, 1, 1),
 (18, 22, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `auth_user_user_permissions`
---
-
-CREATE TABLE `auth_user_user_permissions` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `permission_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `django_admin_log`
---
-
-CREATE TABLE `django_admin_log` (
-  `id` int(11) NOT NULL,
-  `action_time` datetime(6) NOT NULL,
-  `object_id` longtext,
-  `object_repr` varchar(200) NOT NULL,
-  `action_flag` smallint(5) UNSIGNED NOT NULL,
-  `change_message` longtext NOT NULL,
-  `content_type_id` int(11) DEFAULT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `django_admin_log`
@@ -221,18 +124,6 @@ INSERT INTO `django_admin_log` (`id`, `action_time`, `object_id`, `object_repr`,
 (35, '2016-05-04 19:46:49.291000', '15', 'Je suis une musique de toto duree: 0:02:30', 3, '', 12, 4),
 (36, '2016-05-06 08:38:46.839000', '16', 'Je suis une musique de toto duree: 0:02:30', 3, '', 12, 4);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `django_content_type`
---
-
-CREATE TABLE `django_content_type` (
-  `id` int(11) NOT NULL,
-  `app_label` varchar(100) NOT NULL,
-  `model` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `django_content_type`
 --
@@ -249,19 +140,6 @@ INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
 (11, 'workspace', 'tag'),
 (10, 'workspace', 'registration'),
 (13, 'workspace', 'album');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `django_migrations`
---
-
-CREATE TABLE `django_migrations` (
-  `id` int(11) NOT NULL,
-  `app` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `applied` datetime(6) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `django_migrations`
@@ -296,36 +174,12 @@ INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
 (26, 'workspace', '0012_auto_20160504_2111', '2016-05-04 19:11:33.141000'),
 (27, 'workspace', '0013_auto_20160504_2133', '2016-05-04 19:34:01.962000');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `django_session`
---
-
-CREATE TABLE `django_session` (
-  `session_key` varchar(40) NOT NULL,
-  `session_data` longtext NOT NULL,
-  `expire_date` datetime(6) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `django_session`
 --
 
 INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
 ('135l5gvl15rc1tiyx3q91ephwjjv2c20', 'YjA4NTQwNGY1MGVhODFkYzFkMzI4NjYxMmRkYTMzYmEzMGExOTcwMzp7Il9hdXRoX3VzZXJfaGFzaCI6IjY1YjBjZDgyOGUwNTk0NzllYzdjYzY5NGY2OTM0Y2UzMWJhNWM2NGYiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaWQiOiI0In0=', '2016-05-17 07:22:16.268000');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `django_site`
---
-
-CREATE TABLE `django_site` (
-  `id` int(11) NOT NULL,
-  `domain` varchar(100) NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `django_site`
@@ -334,42 +188,12 @@ CREATE TABLE `django_site` (
 INSERT INTO `django_site` (`id`, `domain`, `name`) VALUES
 (2, 'example.com', 'example.com');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `workspace_album`
---
-
-CREATE TABLE `workspace_album` (
-  `id` int(11) NOT NULL,
-  `titre` varchar(100) NOT NULL,
-  `date_publication` date NOT NULL,
-  `type_album` varchar(2) NOT NULL,
-  `artiste_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `workspace_album`
 --
 
 INSERT INTO `workspace_album` (`id`, `titre`, `date_publication`, `type_album`, `artiste_id`) VALUES
 (1, 'La terre est ronde', '2016-05-04', 'AL', 22);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `workspace_music`
---
-
-CREATE TABLE `workspace_music` (
-  `id` int(11) NOT NULL,
-  `titre` varchar(100) NOT NULL,
-  `duree` bigint(20) NOT NULL,
-  `path` varchar(100) NOT NULL,
-  `album_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  `auteur_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `workspace_music`
@@ -378,29 +202,6 @@ CREATE TABLE `workspace_music` (
 INSERT INTO `workspace_music` (`id`, `titre`, `duree`, `path`, `album_id`, `tag_id`, `auteur_id`) VALUES
 (17, 'Balanced Boy', 210000000, 'toto/La terre est ronde/4-5-2016/Millencolin_-_Balanced_Boy_Full_Album_Stream.mp3', 1, 1, 22);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `workspace_registration`
---
-
-CREATE TABLE `workspace_registration` (
-  `id` int(11) NOT NULL,
-  `key` varchar(100) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `workspace_tag`
---
-
-CREATE TABLE `workspace_tag` (
-  `id` int(11) NOT NULL,
-  `intitule` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Contenu de la table `workspace_tag`
 --
@@ -408,197 +209,6 @@ CREATE TABLE `workspace_tag` (
 INSERT INTO `workspace_tag` (`id`, `intitule`) VALUES
 (1, 'Rock');
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `auth_group`
---
-ALTER TABLE `auth_group`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Index pour la table `auth_group_permissions`
---
-ALTER TABLE `auth_group_permissions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `auth_group_permissions_group_id_0cd325b0_uniq` (`group_id`,`permission_id`),
-  ADD KEY `auth_group_permissions_0e939a4f` (`group_id`),
-  ADD KEY `auth_group_permissions_8373b171` (`permission_id`);
-
---
--- Index pour la table `auth_permission`
---
-ALTER TABLE `auth_permission`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `auth_permission_content_type_id_01ab375a_uniq` (`content_type_id`,`codename`),
-  ADD KEY `auth_permission_417f1b1c` (`content_type_id`);
-
---
--- Index pour la table `auth_user`
---
-ALTER TABLE `auth_user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Index pour la table `auth_user_groups`
---
-ALTER TABLE `auth_user_groups`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `auth_user_groups_user_id_94350c0c_uniq` (`user_id`,`group_id`),
-  ADD KEY `auth_user_groups_e8701ad4` (`user_id`),
-  ADD KEY `auth_user_groups_0e939a4f` (`group_id`);
-
---
--- Index pour la table `auth_user_user_permissions`
---
-ALTER TABLE `auth_user_user_permissions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `auth_user_user_permissions_user_id_14a6b632_uniq` (`user_id`,`permission_id`),
-  ADD KEY `auth_user_user_permissions_e8701ad4` (`user_id`),
-  ADD KEY `auth_user_user_permissions_8373b171` (`permission_id`);
-
---
--- Index pour la table `django_admin_log`
---
-ALTER TABLE `django_admin_log`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `django_admin_log_417f1b1c` (`content_type_id`),
-  ADD KEY `django_admin_log_e8701ad4` (`user_id`);
-
---
--- Index pour la table `django_content_type`
---
-ALTER TABLE `django_content_type`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `django_content_type_app_label_76bd3d3b_uniq` (`app_label`,`model`);
-
---
--- Index pour la table `django_migrations`
---
-ALTER TABLE `django_migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `django_session`
---
-ALTER TABLE `django_session`
-  ADD PRIMARY KEY (`session_key`),
-  ADD KEY `django_session_de54fa62` (`expire_date`);
-
---
--- Index pour la table `django_site`
---
-ALTER TABLE `django_site`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `django_site_domain_a2e37b91_uniq` (`domain`);
-
---
--- Index pour la table `workspace_album`
---
-ALTER TABLE `workspace_album`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `workspace_album_24fa59d8` (`artiste_id`);
-
---
--- Index pour la table `workspace_music`
---
-ALTER TABLE `workspace_music`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `workspace_music_95c3b9df` (`album_id`),
-  ADD KEY `workspace_music_76f094bc` (`tag_id`),
-  ADD KEY `workspace_music_2cc46b4c` (`auteur_id`);
-
---
--- Index pour la table `workspace_registration`
---
-ALTER TABLE `workspace_registration`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `workspace_registration_e8701ad4` (`user_id`);
-
---
--- Index pour la table `workspace_tag`
---
-ALTER TABLE `workspace_tag`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `auth_group`
---
-ALTER TABLE `auth_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pour la table `auth_group_permissions`
---
-ALTER TABLE `auth_group_permissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `auth_permission`
---
-ALTER TABLE `auth_permission`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
---
--- AUTO_INCREMENT pour la table `auth_user`
---
-ALTER TABLE `auth_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT pour la table `auth_user_groups`
---
-ALTER TABLE `auth_user_groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT pour la table `auth_user_user_permissions`
---
-ALTER TABLE `auth_user_user_permissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `django_admin_log`
---
-ALTER TABLE `django_admin_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
---
--- AUTO_INCREMENT pour la table `django_content_type`
---
-ALTER TABLE `django_content_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT pour la table `django_migrations`
---
-ALTER TABLE `django_migrations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
---
--- AUTO_INCREMENT pour la table `django_site`
---
-ALTER TABLE `django_site`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `workspace_album`
---
-ALTER TABLE `workspace_album`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pour la table `workspace_music`
---
-ALTER TABLE `workspace_music`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
---
--- AUTO_INCREMENT pour la table `workspace_registration`
---
-ALTER TABLE `workspace_registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT pour la table `workspace_tag`
---
-ALTER TABLE `workspace_tag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
